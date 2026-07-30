c828459193e55d1bbb8e3474a04aa572bcc7417c
Leandro Marques <leandrommarquess.n@gmail.com>
2026-07-30 12:15:01 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   backend/config/db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,6 +9,9 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
+    // --- ADICIONE ESTA LINHA PARA O AIVEN ACEITAR A CONEXÃO ---
+    ssl: { rejectUnauthorized: false },
+    // --- FUSO HORÁRIO ---
     // Configurações de fuso horário
     timezone: '-03:00',
     dateStrings: true,
