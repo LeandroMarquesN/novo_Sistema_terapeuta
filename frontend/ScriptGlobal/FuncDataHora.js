197cfc80594f7214674eb84ab6d72ca3b09eac60
Leandro Marques <leandrommarquess.n@gmail.com>
2026-09-23 23:03:50 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   frontend/ScriptGlobal/FuncDataHora.js

--- frontend/ScriptGlobal/FuncDataHora.js
+++ frontend/ScriptGlobal/FuncDataHora.js
@@ -1,11 +1,16 @@
 // ======  Função data e hora
+// ======  Função data e hora
 function atualizarDataHora() {
+  const elemento = document.getElementById('data-hora');
+  if (!elemento) return;
+
   const now = new Date();
   const dataHora = now.toLocaleString('pt-BR', {
     weekday: 'long', day: '2-digit', month: 'long', year: 'numeric',
     hour: '2-digit', minute: '2-digit', second: '2-digit'
   });
-  document.getElementById('data-hora').textContent = dataHora;
+  elemento.textContent = dataHora;
 }
+
 atualizarDataHora();
 setInterval(atualizarDataHora, 1000);
